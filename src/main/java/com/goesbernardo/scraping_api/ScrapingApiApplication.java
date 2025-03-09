b3e3d36ca0e71e64c25d484efd3044132492beb3
Bernardo Goes <bernardo.goes@infobase.com.br>
2025-03-09 11:46:58 -0300

    first commit

---
+++ src/main/java/com/goesbernardo/scraping_api/ScrapingApiApplication.java
@@ -0,0 +1,13 @@
+package com.goesbernardo.scraping_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrapingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goesbernardo/scraping_api/ScrapingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goesbernardo.scraping_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
